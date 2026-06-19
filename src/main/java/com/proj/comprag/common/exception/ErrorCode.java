c7e9bf45be13e4f325941c3715b66cde7fe78ece
JieunHong <wpluscod@gmail.com>
2026-06-19 21:02:15 +0900

    예외 처리 정리

--- src/main/java/com/proj/comprag/common/exception/ErrorCode.java
+++ src/main/java/com/proj/comprag/common/exception/ErrorCode.java
@@ -6,10 +6,17 @@ public enum ErrorCode {
 
     // 400
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "VALIDATION_ERROR", "요청 값이 올바르지 않습니다."),
+
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "잘못된 요청입니다."),
 
-    // 401
+    // 401 - 토큰 없음/만료
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "인증이 필요합니다."),
+
+    // 401 - 로그인 실패 - 같은 HTTP상태코드이더라도 원인 구분을 위해 분류
+    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "LOGIN_FAILED", "로그인 실패"),
+
+    //401 - 로그인 필요 안내
+//    LOGIN_NEEDED(),
 
     // 403
     FORBIDDEN(HttpStatus.FORBIDDEN, "FORBIDDEN", "권한이 없습니다."),
@@ -19,6 +26,7 @@ public enum ErrorCode {
 
     // 500
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "서버 오류가 발생했습니다.");
+
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/proj/comprag/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proj/comprag/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.proj.comprag.common.exception;
 
+import com.proj.comprag.common.exception.custom.BadRequestException;
+import com.proj.comprag.common.exception.custom.ForbiddenException;
+import com.proj.comprag.common.exception.custom.NotFoundException;
+import com.proj.comprag.common.exception.custom.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +16,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 404 - 문서없음
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiErrorResponse> handleNotFound(
-                                             NotFoundException e,
-                                            HttpServletRequest req) {
-        return build(e.getErrorCode(), e.getMessage(), req);
-    }
-
-    // 403
-    @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<ApiErrorResponse> handleForbidden(ForbiddenException e, HttpServletRequest req) {
-        return build(e.getErrorCode(), e.getMessage(), req);
-    }
 
     // 400
     @ExceptionHandler(BadRequestException.class)
@@ -32,7 +23,8 @@ public class GlobalExceptionHandler {
         return build(e.getErrorCode(), e.getMessage(), req);
     }
 
-    // 400 - @Valid 검증 실패,
+    // Spring 기본 예외를 apiErrorResponse로 처리
+    // 400 - @Valid 검증 실패
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleValidation(
             MethodArgumentNotValidException e, HttpServletRequest req) {
@@ -43,11 +35,34 @@ public class GlobalExceptionHandler {
         return build(ErrorCode.VALIDATION_ERROR, msg, req);
     }
 
-    // 400 - 네가 방금 겪은 케이스도 400으로 내려주기 좋음
+    // 400
     @ExceptionHandler(MissingPathVariableException.class)
     public ResponseEntity<ApiErrorResponse> handleMissingPathVar(MissingPathVariableException e, HttpServletRequest req) {
         return build(ErrorCode.BAD_REQUEST, "요청 경로 변수(path variable)가 누락되었습니다.", req);
     }
+
+    // 401 - 문서없음
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ApiErrorResponse> handleNotFound(
+                                             UnauthorizedException e,
+                                            HttpServletRequest req) {
+        return build(e.getErrorCode(), e.getMessage(), req);
+    }
+
+    // 404 - 문서없음
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleNotFound(
+            NotFoundException e,
+            HttpServletRequest req) {
+        return build(e.getErrorCode(), e.getMessage(), req);
+    }
+
+    // 403
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ApiErrorResponse> handleForbidden(ForbiddenException e, HttpServletRequest req) {
+        return build(e.getErrorCode(), e.getMessage(), req);
+    }
+
 
     // 나머지 전부 500
     @ExceptionHandler(Exception.class)
@@ -58,6 +73,13 @@ public class GlobalExceptionHandler {
 
         return build(ErrorCode.INTERNAL_ERROR, ErrorCode.INTERNAL_ERROR.getDefaultMessage(), req);
     }
+
+//    @ExceptionHandler()
+//    public ResponseEntity<ApiErrorResponse> handleUnauthorized(Exception e, HttpServletRequest req) {
+//        return build()
+//    }
+
+
 
     private ResponseEntity<ApiErrorResponse> build(ErrorCode errorCode, String message, HttpServletRequest req) {
         int status = errorCode.getStatus().value();
